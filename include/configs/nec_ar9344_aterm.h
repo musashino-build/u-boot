7e55c1d3f3e59d72b60fa0e2b6fcbc6c32839212
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-04-24 23:54:46 +0900

    ath79: add support for NEC AR9344 Aterm series

---
+++ include/configs/nec_ar9344_aterm.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2024 INAGAKI Hiroshi <musashino.open@gmail.com>
+ */
+
+#ifndef __NEC_AR9344_ATERM_H
+#define __NEC_AR9344_ATERM_H
+
+#define CFG_SYS_SDRAM_BASE		0x80000000
+
+#define CFG_SYS_INIT_RAM_ADDR	0xbd000000
+#define CFG_SYS_INIT_RAM_SIZE	0x8000
+
+/*
+ * Serial Port
+ */
+#define CFG_SYS_NS16550_CLK		40000000
+
+/*
+ * Command
+ */
+/* Miscellaneous configurable options */
+
+/*
+ * Diagnostics
+ */
+
+#endif  /* __NEC_AR9344_ATERM_H */
