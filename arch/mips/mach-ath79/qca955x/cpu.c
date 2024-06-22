636b643d15bfd44862cc5131c6c176046a56b923
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-06-22 20:45:31 +0900

    mips: ath79: add initial support for QCA955x SoCs
    mips: ath79: fix PLL initialization for QCA955x
    
    mips: ath79: add 32bit DDR width support
    
    mips: ath79: improve DDR initialization on QCA955x with FIELD_PREP
    
    mips: ath79: adjust indentation and cleanup QCA955x support
    
    mips: ath79: fix handling of (CPU|DDR)CLK_FROM_(CPU|DDR)PLL
    
    mips: ath79: use 240MHz AHB clock instead of 300MHz

---
+++ arch/mips/mach-ath79/qca955x/cpu.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2024 INAGAKI Hiroshi <musashino.open@gmail.com>
+ */
+
+/* The lowlevel_init() is not needed on QCA955X */
+void lowlevel_init(void) {}
